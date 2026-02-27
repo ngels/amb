7e5e519be56850f06f7724590734b071210fb1c3
tokamak <pamyngels@outlook.fr>
2026-02-27 16:40:10 +0800

    Update BASE_URL to use environment variable exclusively
    Remove default BASE_URL fallback to use environment variable only.

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://internal-prodst-ambBE-rm4CvraJKNga-2040253924.ap-northeast-3.elb.amazonaws.com/api/v1'
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
 
